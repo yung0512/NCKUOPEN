9e8edcbaf49480de0a0df7dbcfed3e5ec10ebded
yung0512 <jeff8651215@gmail.com>
2022-05-24 19:54:44 +0800

    add typescript in frontend

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>This is the Home Page of NCKU</h1>
+      <p>Yeah!</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/pages/sign_in.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+
+const SingIN:NextPage = () => {
+
+  return (
+    <div>
+      <h1>SingIN</h1>
+      <input 
+        className="email"
+        type="email"
+      />
+    </div>
+  )
+}
+
+export default SingIN;
