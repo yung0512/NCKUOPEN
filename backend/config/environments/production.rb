34a74a1f7d704af54e6cd4795a4870b37f8fe2d4
yung0512 <jeff8651215@gmail.com>
2022-05-24 23:20:35 +0800

    fix

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -75,7 +75,6 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.hosts << "backend"
   
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
