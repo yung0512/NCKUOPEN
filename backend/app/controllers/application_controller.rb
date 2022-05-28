949d0caf6f80a311e171e02f450d19529f5b7a97
yung0512 <jeff8651215@gmail.com>
2022-05-28 16:04:25 +0800

    update

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def index
+    render(json: { success: true })
+  end
 end
--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -74,14 +74,15 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
-
+  config.hosts << '139.162.112.224'
+  config.hosts << 'nckuopen.com'
   
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.hosts << "backend"
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
